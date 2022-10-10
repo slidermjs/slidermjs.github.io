643ad35359c42977917df608f784dda5996e290d
Terry Lin <terrylin.developer@gmail.com>
2022-10-10 19:14:10 +0800

    Update Initialize Jekyll site.

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+(()=>{var __webpack_modules__={"./js/index.js":
+/*!*********************!*\
+  !*** ./js/index.js ***!
+  \*********************/()=>{eval("\n\n//# sourceURL=webpack://@sliderm.js/doc/./js/index.js?")},"./scss/index.scss":
+/*!*************************!*\
+  !*** ./scss/index.scss ***!
+  \*************************/(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{"use strict";eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://@sliderm.js/doc/./scss/index.scss?")}},__webpack_require__={r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}};__webpack_modules__["./js/index.js"](0,{},__webpack_require__);var __webpack_exports__={};__webpack_modules__["./scss/index.scss"](0,__webpack_exports__,__webpack_require__)})();
